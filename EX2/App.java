16ea0ab9b38115705cffb9e002c11c5b0ac964ee
PilouSquaglia <pilou.squaglia@laposte.net>
2021-12-14 15:16:48 +0100

    14/12/21

--- EX2/App.java
+++ EX2/App.java
@@ -1,14 +1,33 @@
 package EX2;
 import java.time.LocalDate;
 import EX2.Citerne.Liquide;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        LocalDate dateMiseService=LocalDate.of(2021,11,12);
-        Liquide Vin = Liquide.Vin;
+        ArrayList<Citerne> citernes=new ArrayList<Citerne>();
 
-        Citerne c1 = new Citerne(13, 9.5, Vin, dateMiseService);
-        System.out.println(c1);
+        LocalDate dateMiseService1=LocalDate.of(2021,12,11);
+        LocalDate dateMiseService2=LocalDate.of(2021,8,5);
+        LocalDate dateMiseService3=LocalDate.of(2021,1,31);
+
+        Liquide Vin = Liquide.Vin;
+        Liquide Eau = Liquide.Eau;
+        Liquide Huile = Liquide.Huile;
+
+        Citerne c1 = new Citerne(13, 9.5, Eau, dateMiseService1);
+        Citerne c2 = new Citerne(13, 9.5, Eau, dateMiseService1);
+        Citerne c3 = new Citerne(13, 9, Eau, dateMiseService1);
+
+        System.out.println(c1.equals(c2));
+        System.out.println(c1.equals(c3));
+
+        // citernes.add(new Citerne(13, 9.5, Eau, dateMiseService1));
+        // citernes.add(new Citerne(8, 6.2, Vin, dateMiseService2));
+        // citernes.add(new Citerne(15, 10, Huile, dateMiseService3));
+        // citernes.add(new Citerne(0, -10, null, dateMiseService3));
+        // citernes.forEach(citerne -> System.out.println(citerne));
+
     }
 }
--- EX2/Citerne.java
+++ EX2/Citerne.java
@@ -3,10 +3,13 @@ import java.time.LocalDate;
 
 public class Citerne {
 
-    private static int id=0;
+    private static int nbCiterne=0;
+    private String id="";
     private int capacite;
     private LocalDate dateMiseService;
     private double volume;
+    private Liquide liquide;
+
     public enum Liquide {
         Eau(10),
         Vin(15),
@@ -23,12 +26,13 @@ public class Citerne {
     }
 
     public Citerne(int capacite, double volume, Liquide liquide, LocalDate dateMiseService){
-        id++;
+        nbCiterne++;
+        id+=nbCiterne;
         try{
             if(capacite>0)
                 this.capacite=capacite;
         }
-        catch(Exception e){
+        catch(IllegalArgumentException e){
             System.err.println("Erreur dans la saisie de l'argument, la capacité doit être positive");
         }
 
@@ -36,18 +40,20 @@ public class Citerne {
             if(volume>0)
                 this.volume=volume;
         }
-        catch(Exception e){
+        catch(IllegalArgumentException e){
             System.err.println("Erreur dans la saisie de l'argument, le volume doit être positive");
         }
 
         this.dateMiseService=dateMiseService;
 
         try{
-            if(liquide!=Vin) || (liquide!=Eau) || (liquide!=Huile)
-                Liquide typeLiquide = Liquide.liquide;
+            if(liquide==Liquide.Vin || liquide==Liquide.Eau || liquide==Liquide.Huile)
+                this.liquide=liquide;
         }
-       
-        //Liquide liquide = liquide;
+        catch(IllegalArgumentException e){
+            System.err.println("Erreur dans la saisie de l'argument, le liquide est soit de l'eau, du vin ou de l'huile");
+        }
+
     }
 
     public boolean plusAncienne(Citerne c2){
@@ -56,6 +62,86 @@ public class Citerne {
 
     @Override
     public String toString() {
-        return "Citerne n°"+id+", %s, capacité : "+capacite+" m3, mise en service : "+dateMiseService+", volume occupé : "+volume;
+        return "Citerne n°"+id+", "+liquide+", capacité : "+capacite+" m³, mise en service : "+dateMiseService+", volume occupé : "+volume+" m³";
     }
+
+    public void nettoyage(){
+        this.liquide=null;
+    }
+
+    public void ajouterLiquide(double addquantite){
+        double quantite_final;
+        if(addquantite>0 && addquantite<1)
+            quantite_final=volume+capacite*(addquantite);
+        else{
+            quantite_final=this.volume;
+        }
+        try{
+            if(quantite_final<=capacite && this.liquide!=null)
+                this.volume=quantite_final;
+        }
+        catch(IllegalArgumentException e){
+            System.err.println("Erreur dans la saisie de l'argument, le volume occupé est supérieur à la capacité de la cuve, ou bien la cuve n'a pas encore de type de liquide assigné");
+        }
+    }
+
+    public void ajouterLiquide(int addquantite){
+        double quantite_final;
+        if(addquantite>0)
+            quantite_final=volume+addquantite;
+        else{
+            quantite_final=this.volume;
+        }
+        try{
+            if(quantite_final<=capacite && this.liquide!=null)
+                this.volume=quantite_final;
+        }
+        catch(IllegalArgumentException e){
+            System.err.println("Erreur dans la saisie de l'argument, le volume occupé est supérieur à la capacité de la cuve, ou bien la cuve n'a pas encore de type de liquide assigné");
+        }
+    }
+
+    public void enleverLiquide(double suppquantite){
+        double quantite_final;
+        if(suppquantite>0 && suppquantite<1)
+            quantite_final=volume-capacite*(suppquantite);
+        else{
+            quantite_final=this.volume;
+        }
+        try{
+            if(quantite_final>=0 && this.liquide!=null)
+                this.volume=quantite_final;
+        }
+        catch(IllegalArgumentException e){
+            System.err.println("Erreur dans la saisie de l'argument, le volume occupé demandé est inférieur à 0 il manque "+ Math.abs(quantite_final) +", ou bien la cuve n'a pas encore de type de liquide assigné");
+            this.volume=0;
+        }
+    }
+
+    public void enleverLiquide(int suppquantite){
+        double quantite_final;
+        if(suppquantite>0)
+            quantite_final=volume-suppquantite;
+        else{
+            quantite_final=this.volume;
+        }
+        try{
+            if(quantite_final>=0 && this.liquide!=null)
+                this.volume=quantite_final;
+        }
+        catch(IllegalArgumentException e){
+            System.err.println("Erreur dans la saisie de l'argument, le volume occupé demandé est inférieur à 0 il manque "+ Math.abs(quantite_final) +", ou bien la cuve n'a pas encore de type de liquide assigné");
+            this.volume=0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj != null && obj instanceof Citerne
+        && ((Citerne)obj).capacite == this.capacite
+        && ((Citerne)obj).dateMiseService.equals(this.dateMiseService)
+        && ((Citerne)obj).liquide == this.liquide
+        && ((Citerne)obj).volume == this.volume;
+    }
+
 }
